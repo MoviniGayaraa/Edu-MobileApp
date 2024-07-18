500086fb7a4b2970bbc47250ea99471bd4a4ad49
MoviniGayaraa <movinigayara32@gmail.com>
2024-07-18 20:17:10 +0530

    updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,20 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useFonts } from 'expo-font';
+
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import 'react-native-reanimated';
+import OnBoarding from './(routes)/onboarding';
+import { Stack } from 'expo-router';
+
+
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -46,14 +44,24 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  
+  const [isLoggedIn, setisLoggedIn] = useState(false);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <>
+     {isLoggedIn ? (
+      <view></view>
+     ):(
+     <Stack screenOptions={{headerShown:false}}>
+      <Stack.Screen name ="index" />
+      <Stack.Screen name ="(routes)/welcome-intro/index" />
+      <Stack.Screen name="(routes)/login/index" />
+     </Stack>
+   
+
+   
+  )}
+  </>
   );
+
 }
